b669cf49c9a3889a691c3b53d7417ea7531d9d86
Moises Hernandez <mhdezcoronado@gmail.com>
2021-07-13 12:28:27 +0200

    Load html with webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
+   output: {
+     filename: 'dist/index.html',
+     filename: 'main.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+};
